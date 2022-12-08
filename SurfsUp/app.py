508f5c31d0c94081a6cd261a3ca18822079d63ad
Brianna <oconnor.brianna@gmail.com>
2022-12-08 13:39:37 +0800

    tobs function added

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -2,17 +2,15 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
-
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
 
 # Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-
+conn = engine.connect()
 Base = automap_base()
 Base.prepare(autoload_with=engine)
 
@@ -20,7 +18,7 @@ Base.prepare(autoload_with=engine)
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Create an app, being sure to pass __name__
+# Create an app
 app = Flask(__name__)
 
 # 1. Define what to do when a user hits the index route
@@ -41,6 +39,7 @@ def home():
 # 2. Convert the query results to a dictionary by using date as the key and prcp as the value.
 #    Return the JSON representation of your dictionary.
 
+
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     print("Server received request for precipitation information ...")
@@ -49,30 +48,38 @@ def precipitation():
         filter(Measurement.date > dt.datetime(2016, 8, 23)
                ).order_by(Measurement.date.desc())
     session.close()
-    
     precip = {date: prcp for date, prcp in results_output}
     return jsonify(precip)
 
+# 3. Return a JSON list of stations from the dataset.
 
-#     results = list(np.ravel(results_output))
-    
-    
-#     for result in results_output:
-        
-#         print(f"{result._asdict()}")
 
-#     results = list(np.ravel(results_output))
-#     return jsonify(results)
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+    st_count = session.query(
+        Station.station, Station.name).group_by(Station.station)
+    session.close()
+    # station_list = list(np.ravel(st_count))
+    print("working on station query")
+    station_info = {stn: nm for stn, nm in st_count}
+    return jsonify(station_info)
+#
+# 4. Query the dates and temperature observations of the most-active station for the previous year of data.
+#   Return a JSON list of temperature observations for the previous year.
 
-    
-    # data = [((f"{result[0]}, {result[1]}") for result in results_output)]
-    # precip = {key: value for (key, value) in data}   
-    # return jsonify(precip)
-# two methods _____________
-#     prior12_rows = [{"Date": result[0], "Precipitation": result[1]}
-#                 for result in results_output]
-#     return (f"{prior12_rows}")
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    print("working on tobs query")
+    session = Session(engine)
+    active_12 = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date > dt.datetime(2016, 8, 18)).filter(Measurement.station == "USC00519281").\
+        order_by(Measurement.date)
+    session.close()
+    active12_output = {day: tob for day, tob in active_12}
+    return jsonify(active12_output)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-  
